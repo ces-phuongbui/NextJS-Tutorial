1d206d5dead6c3f1dde5016648eb2c3374a1b029
ces-phuongbui <phuong.bui@codeenginestudio.com>
2023-11-13 15:45:52 +0700

    test

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,10 @@
 import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
-  pageShots: {
-    pages: [{ path: "/", name: "landing" }],
-    baseUrl: "http://172.17.0.1:3000",
+  storybookShots: {
+    storybookUrl: "./storybook-static",
   },
+
   lostPixelProjectId: "clowcrkf92s94g80eogqsmn5j",
   apiKey: process.env.LOST_PIXEL_API_KEY,
   generateOnly: true,
