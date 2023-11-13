a50f0813112d1e6b9e6ca3e072a895779dab4e81
ces-phuongbui <phuong.bui@codeenginestudio.com>
2023-11-13 15:26:04 +0700

    test

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,11 +2,22 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: "/", name: "landing" }],
-    baseUrl: "http://172.17.0.1:3000",
+    pages: [
+      { path: "/app", name: "app" },
+      { path: "/next-app", name: "next-app" },
+      { path: "/next-app?name=App", name: "next-app-with-query-param" },
+      { path: "/fetch", name: "fetch-static-props" },
+      { path: "/client-fetch", name: "fetch-client" },
+    ],
+    // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
+
+    baseUrl: "http://localhost:3000",
   },
-  lostPixelProjectId: "clowcrkf92s94g80eogqsmn5j",
-  apiKey: process.env.LOST_PIXEL_API_KEY,
+  // OSS mode
   generateOnly: true,
   failOnDifference: true,
+
+  // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
+  // lostPixelProjectId: "xxxx",
+  // process.env.LOST_PIXEL_API_KEY,
 };
