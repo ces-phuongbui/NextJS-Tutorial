4c5bc8aee3ee8b823f214cf0a51cbadd21367749
ces-phuongbui <phuong.bui@codeenginestudio.com>
2023-11-13 17:09:03 +0700

    test js file

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pageShots: {
+    pages: [ { path: '/', name: 'landing' } ],
+    baseUrl: 'http://172.17.0.1:3000',
+  },
+    lostPixelProjectId: 'clowcrkf92s94g80eogqsmn5j',
+    apiKey: process.env.LOST_PIXEL_API_KEY,
+  };
